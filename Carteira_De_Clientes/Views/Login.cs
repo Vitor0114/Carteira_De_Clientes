7a0d980970f0c17c685fc44a81b130d4b59d638f
Vitor0114 <vitorgustav0114@gmail.com>
2023-06-27 15:45:12 -0300

    Esqueleto

---
+++ Carteira_De_Clientes/Views/Login.cs
@@ -0,0 +1,15 @@
+namespace Carteira_De_Clientes;
+
+public partial class Login : Form
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Form1";
+    }
+}
